fc06093c6891b12b348695e8e1124a7504dc4cb9
Pablo Franco <pablo.francopinilla.emeal.st@emeal.nttdata.com>
2023-10-07 09:51:24 +0200

    Solar System Terminado

--- PARTE DEL SISTEMA SOLAR/zoom.js
+++ PARTE DEL SISTEMA SOLAR/zoom.js
@@ -26,69 +26,85 @@ function ocultarPaginaPorId(idElemento) {
             if (idElemento == "mercure") {
                 divUnBlock.style.backgroundImage = 'url("./planetas/mercurio.png")';
                 divUnBlock.innerHTML = `
+                <img class="arrow" src="img/flecha.png" alt="Flecha atrás"  onclick="volver()">
             <h2 class="EngName">${data.englishName}</h2>
             <p class="Diam">Diámetro: ${data.meanRadius} km</p>
             <p class="gravity">Gravedad:  ${data.gravity} N</p>
-            <p class="Temp">Temperatura Media: (${(data.avgTemp) - 273.15})</p>
+            <p class="Temp">Temperatura Media: ${(data.avgTemp) - 273.15} ºC &#128520</p>
+            <p class="Density">Densidad: (${(data.density)})</p>
                 
             `;
             } else if (idElemento == "venus") {
                 divUnBlock.style.backgroundImage = 'url("./planetas/venus.png")';
                 divUnBlock.innerHTML = `
+                <img class="arrow" src="img/flecha.png" alt="Flecha atrás"  onclick="volver()">
             <h2 class="EngName">${data.englishName}</h2>
             <p class="Diam">Diámetro: ${data.meanRadius} km</p>
             <p class="gravity">Gravedad:  ${data.gravity} N</p>
-            <p class="Temp">Temperatura Media: (${(data.avgTemp) - 273.15})</p>
+            <p class="Temp">Temperatura Media: ${(data.avgTemp) - 273.15} ºC &#128520 &#128520</p>
+            <p class="Density">Densidad: (${(data.density)})</p>
                
             `;
             } else if (idElemento == "terre") {
                 divUnBlock.style.backgroundImage = 'url("./planetas/tierra.png")';
                 divUnBlock.innerHTML = `
+                <img class="arrow" src="img/flecha.png" alt="Flecha atrás"  onclick="volver()">
             <h2 class="EngName">${data.englishName}</h2>
             <p class="Diam">Diámetro: ${data.meanRadius} km</p>
             <p class="gravity">Gravedad:  ${data.gravity} N</p>
-            <p class="Temp">Temperatura Media: (${(data.avgTemp) - 273.15})</p>
+            <p class="Temp">Temperatura Media: ${(data.avgTemp) - 273.15} ºC &#127757</p>
+            <p class="Density">Densidad: (${(data.density)})</p>
               
             `;
             } else if (idElemento == "mars") {
                 divUnBlock.style.backgroundImage = 'url("./planetas/marte.png")';
                 divUnBlock.innerHTML = `
+                <img class="arrow" src="img/flecha.png" alt="Flecha atrás"  onclick="volver()">
             <h2 class="EngName">${data.englishName}</h2>
             <p class="Diam">Diámetro: ${data.meanRadius} km</p>
             <p class="gravity">Gravedad:  ${data.gravity} N</p>
-            <p class="Temp">Temperatura Media: (${(data.avgTemp) - 273.15})</p>
+            <p class="Temp">Temperatura Media: ${(data.avgTemp) - 273.15} ºC &#10052</p>
+            <p class="Density">Densidad: (${(data.density)})</p>
             `;
             } else if (idElemento == "jupiter") {
                 divUnBlock.style.backgroundImage = 'url("./planetas/jupiter.png")';
                 divUnBlock.innerHTML = `
+                <img class="arrow" src="img/flecha.png" alt="Flecha atrás"  onclick="volver()">
             <h2 class="EngName">${data.englishName}</h2>
             <p class="Diam">Diámetro: ${data.meanRadius} km</p>
             <p class="gravity">Gravedad:  ${data.gravity} N</p>
-            <p class="Temp">Temperatura Media: (${(data.avgTemp) - 273.15})</p>
+            <p class="Temp">Temperatura Media: ${(data.avgTemp) - 273.15} ºC &#10052 &#10052</p>
+            <p class="Density">Densidad: (${(data.density)})</p>
             `;
             } else if (idElemento == "saturne") {
                 divUnBlock.style.backgroundImage = 'url("./planetas/saturno.png")';
                 divUnBlock.innerHTML = `
+                <img class="arrow" src="img/flecha.png" alt="Flecha atrás"  onclick="volver()">
                 <h2 class="EngName">${data.englishName}</h2>
                 <p class="Diam">Diámetro: ${data.meanRadius} km</p>
                 <p class="gravity">Gravedad:  ${data.gravity} N</p>
-                <p class="Temp">Temperatura Media: (${(data.avgTemp) - 273.15})</p>
+                <p class="Temp">Temperatura Media: ${(data.avgTemp) - 273.15} ºC &#10052 &#10052</p>
+                <p class="Density">Densidad: (${(data.density)})</p>
             `;
             } else if (idElemento == "uranus") {
                 divUnBlock.style.backgroundImage = 'url("./planetas/urano.png")';
                 divUnBlock.innerHTML = `
+                <img class="arrow" src="img/flecha.png" alt="Flecha atrás"  onclick="volver()">
             <h2 class="EngName">${data.englishName}</h2>
             <p class="Diam">Diámetro: ${data.meanRadius} km</p>
             <p class="gravity">Gravedad:  ${data.gravity} N</p>
-            <p class="Temp">Temperatura Media: (${(data.avgTemp) - 273.15})</p>
+            <p class="Temp">Temperatura Media: ${(data.avgTemp) - 273.15} ºC &#10052 &#10052 &#10052;</p>
+            <p class="Density">Densidad: (${(data.density)})</p>
             `;
             } else if (idElemento == "neptune") {
                 divUnBlock.style.backgroundImage = 'url("./planetas/neptuno.png")';
                 divUnBlock.innerHTML = `
+                <img class="arrow" src="img/flecha.png" alt="Flecha atrás"  onclick="volver()">
             <h2 class="EngName">${data.englishName}</h2>
             <p class="Diam">Diámetro: ${data.meanRadius} km</p>
             <p class="gravity">Gravedad:  ${data.gravity} N</p>
-            <p class="Temp">Temperatura Media: (${(data.avgTemp) - 273.15})</p>
+            <p class="Temp">Temperatura Media: ${(data.avgTemp) - 273.15} ºC &#10052 &#10052 &#10052</p>
+            <p class="Density">Densidad: (${(data.density)})</p>
             `;
             }
 
@@ -100,7 +116,7 @@ function ocultarPaginaPorId(idElemento) {
             console.error(error);
         });
 }
-function volver(){
+function volver() {
     const divUnBlock = document.getElementById('contenido_block');
     const divBlock = document.getElementById('contenido_home');
     divBlock.style.display = 'block';
